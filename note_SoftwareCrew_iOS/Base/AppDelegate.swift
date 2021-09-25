0df09269e6aa27fa10a5581c9d867e45da72ff5b
kirnabamrahjandu <kirna1851994@gmail.com>
2021-09-25 10:56:10 +0530

    Appdelegate work
    Appdelegate work

--- note_SoftwareCrew_iOS/Base/AppDelegate.swift
+++ note_SoftwareCrew_iOS/Base/AppDelegate.swift
@@ -27,32 +27,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     @available(iOS 13.0, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
     @available(iOS 13.0, *)
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+       
     }
      func applicationWillTerminate(_ application: UIApplication) {
-              // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-              // Saves changes in the application's managed object context before the application terminates.
               self.saveContext()
           }
 
         // MARK: - Core Data stack
     lazy var persistentContainer: NSPersistentContainer = {
 
-       
-            /*
-             The persistent container for the application. This implementation
-             creates and returns a container, having loaded the store for the
-             application to it. This property is optional since there are legitimate
-             error conditions that could cause the creation of the store to fail.
-            */
             let container = NSPersistentContainer(name: "Project_IOS_datamodel")
             container.loadPersistentStores(completionHandler: { (storeDescription, error) in
                 if let error = error as NSError? {
